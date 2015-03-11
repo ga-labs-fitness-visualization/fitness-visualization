c4c19f599cccef5655d2587dbee7e5fc4d93d2f4
Mike Boyle <mikesboyle@gmail.com>
2015-03-11 19:24:43 -0400

    adds temp ajax call with dynamic url

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,8 +7,13 @@ $(function(){
   setTimeout(function(){
     testView.$bar.animate({height: "55%", }, 1000, 'easeInOutBack')
   }, 500);
+    // getting the user ID from the show page URL
+    var a = document.URL
+    var id = a.substring(a.lastIndexOf('/') + 1, a.length);
+    console.log (id);
+    console.log ('/users/' + id);
     $.ajax({
-    url: '/users/1',
+    url: '/users/' + id,
     data: { duration: 30},
     dataType: 'json',
     method: 'GET',
