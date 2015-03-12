5b4f823cd426cad23530f7f714e08c80dd862c95
Mike Boyle <mikesboyle@gmail.com>
2015-03-12 10:18:08 -0400

    added day week month buttons with click handlers and json parsing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,63 @@
 $(function(){
+  //get user id variable from url
+  //kind of a hack but it works!
+  var a = document.URL
+  var id = a.substring(a.lastIndexOf('/') + 1, a.length);
+
+  //these functions get floors and distance out of the json object
+  //just placeholders until this is done in the back end
+  var parseFloors = function(data) {
+    var totalFloors = data["summary"]["floors"];
+    return totalFloors;
+  }
+
+  var parseDistance = function(data) {
+    var totalDistance = data["summary"]["distances"][0]["distance"];
+    return totalDistance;
+  }
+
+  //set click handlers on buttons
+  // success callback function will initiate the object creation/collection/render/animation process
+  $('#get-data-day').click(function(){
+    $.ajax({
+      url: '/users/' + id,
+      data: { duration: 1},
+      dataType: 'json',
+      method: 'GET',
+      success: function(data){
+        console.log(data);
+        console.log('You climbed ' + parseFloors(data) + ' floors.');
+        console.log('You moved ' + parseDistance(data) + ' miles.');
+      }
+    })
+  });
+
+  $('#get-data-week').click(function(){
+    $.ajax({
+      url: '/users/' + id,
+      data: { duration: 7},
+      dataType: 'json',
+      method: 'GET',
+      success: function(data){
+        console.log(data);
+      }
+    })
+  });
+
+  $('#get-data-month').click(function(){
+    $.ajax({
+      url: '/users/' + id,
+      data: { duration: 30},
+      dataType: 'json',
+      method: 'GET',
+      success: function(data){
+        console.log(data);
+      }
+    })    
+  });
+
+  // just testing out getting a building to render and animate on page load
+  // this will NOT be part of the final code
   var esb = new Building();
   var testView = new BuildingView(esb);
   testView.render();
@@ -7,18 +66,5 @@ $(function(){
   setTimeout(function(){
     testView.$bar.animate({height: "55%", }, 1000, 'easeInOutBack')
   }, 500);
-    // getting the user ID from the show page URL
-    var a = document.URL
-    var id = a.substring(a.lastIndexOf('/') + 1, a.length);
-    console.log (id);
-    console.log ('/users/' + id);
-    $.ajax({
-    url: '/users/' + id,
-    data: { duration: 30},
-    dataType: 'json',
-    method: 'GET',
-    success: function(data){
-      console.log(data);
-    }
-  })
+
 })
