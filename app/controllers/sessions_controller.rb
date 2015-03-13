123885516dd6ffdf422c457e17635d4293e9a1e2
Mike Boyle <mikesboyle@gmail.com>
2015-03-13 17:24:08 -0400

    added bcrypt auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,42 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+
+  end
+
+  def create
+    @user = User.find_by_credentials(session_params)
+    if @user
+      token = SecureRandom.urlsafe_base64
+      session[:session_token] = token
+      @user.session_token = token
+      @user.save
+
+      redirect_to @user
+    else
+      @user = User.new(email: session_params[:email])
+
+      render :new
+    end
+  end
+
+  def destroy
+    @user = current_user
+    token = SecureRandom.urlsafe_base64
+    session[:session_token] = token
+    
+    if @user
+      @user.session_token = nil
+      @user.save
+    end
+
+    redirect_to login_path
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -147,6 +147,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params[:user]
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
