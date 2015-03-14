981607acb28aa86dfc2e0419b02f5aee4e64476d
Mike Boyle <mikesboyle@gmail.com>
2015-03-14 16:43:38 -0400

    refactored oauth, made new user signup go straight to login and fitbit oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   
 	helper_method :current_user
+	protect_from_forgery with: :exception
 
 	def current_user
 		return nil unless session[:session_token]
 		User.find_by(session_token: session[:session_token])
 	end
 
+	def get_API_keys
+		begin
+      Fitgem::Client.symbolize_keys(YAML.load(File.open("lib/fitgem.yml")))
+    rescue ArgumentError => e
+      puts "Could not parse YAML: #{e.message}"
+      exit
+    end
+	end
+
+	# is anything still using this method?
   def get_date_strings(num)
 		i = 1
 		date_strings_array = []
@@ -20,5 +31,5 @@ class ApplicationController < ActionController::Base
 		return date_strings_array
 	end 
 	
-  protect_from_forgery with: :exception
+  
 end
--- app/modules/fitbit_accessor.rb
+++ app/modules/fitbit_accessor.rb
@@ -1,6 +1,2 @@
-class FitbitAccessor
 
-  
-
-end
 
