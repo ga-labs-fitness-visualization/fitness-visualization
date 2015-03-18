bb2a7e2015b723aa1c0e0afc927fff3866ce196f
Mike Boyle <mikesboyle@gmail.com>
2015-03-18 09:39:08 -0400

    fixed welcome route so current user goes to fitbit login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
   
   def welcome
     if current_user 
-      redirect_to current_user
+      # redirect_to current_user
+      redirect_to fitbit_login
     else
       render :welcome, layout: false
     end
